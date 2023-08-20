6c2ee1ab10d5d86c12afd51015aa105a66712611
Wajorrr <2434007515@qq.com>
2023-08-20 11:45:14 +0800

    完成lab5-cache lab

--- labs/cachelab/csim.c
+++ labs/cachelab/csim.c
@@ -1,7 +1,246 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+typedef struct cache_line
 {
-    printSummary(0, 0, 0);
+    int valid;         // 有效位
+    int tag;           // 标记位
+    int last_vis_time; // 最近一次访问时间戳
+    char *data_;
+} cache_line_;
+typedef struct cache_set
+{
+    cache_line_ *cache_line;
+} cache_set_;
+typedef struct cache
+{
+    int S; // 包含S个缓存组
+    int E; // 每个缓存组包含的缓存行数量
+    int B; // 每个缓存行包含的字节数量
+    cache_set_ *cache_set;
+} cache_;
+
+void init(cache_ *cache, int s, int E, int b) // C语言没有构造函数，只能单独定义一个初始化函数
+{
+    cache->S = 1 << s;
+    cache->E = E;
+    cache->B = 1 << b;
+    cache->cache_set = (cache_set_ *)malloc(sizeof(cache_set_) * cache->S); // 为S个缓存组申请内存
+    for (int i = 0; i < cache->S; i++)
+    {
+        // 为每个组的E个缓存行申请内存
+        cache->cache_set[i].cache_line = (cache_line_ *)malloc(sizeof(cache_line_) * cache->E);
+        // 缓存行初始化，并为每个缓存行的数据块申请内存
+        for (int j = 0; j < cache->E; j++)
+        {
+            // 本lab中不涉及实际的数据存储，因此不需要给缓存行的数据块申请空间
+            // cache->cache_set[i].cache_line[j].data_ = (char *)malloc(sizeof(char) * cache->B);
+            cache->cache_set[i].cache_line[j].valid = 0;
+            cache->cache_set[i].cache_line[j].tag = 0;
+            cache->cache_set[i].cache_line[j].last_vis_time = 0;
+        }
+    }
+}
+void del(cache_ *cache)
+{
+    for (int i = 0; i < cache->S; i++)
+    {
+        // // 释放每个缓存行的数据块空间
+        // for (int j = 0; j < cache->E; j++)
+        // {
+        //     free(cache->cache_set[i].cache_line[j].data_);
+        // }
+
+        // 释放每个缓存组的缓存行空间
+        free(cache->cache_set[i].cache_line);
+    }
+    free(cache->cache_set); // 释放缓存组空间
+    free(cache);            // 释放缓存指针
+}
+
+// 定义完了数据结构以及初始化、删除函数，接下来把查找缓存、缓存替换需要的函数定义一下
+
+// 查找缓存，参数为缓存组指针cache_set，标记位tag
+int find(cache_ *cache, int set_index, int tag, int time)
+{
+    for (int i = 0; i < cache->E; i++)
+    {
+        if (cache->cache_set[set_index].cache_line[i].valid &&
+            cache->cache_set[set_index].cache_line[i].tag == tag)
+        {
+            cache->cache_set[set_index].cache_line[i].last_vis_time = time;
+            return 1;
+        }
+    }
+    return 0;
+}
+int IsFull(cache_ *cache, int set_index) // 判断cache的编号为set_index的缓存组是否已满
+{
+    for (int i = 0; i < cache->E; i++)
+    {
+        if (cache->cache_set[set_index].cache_line[i].valid == 0)
+            return 0;
+    }
+    return 1;
+}
+// 缓存驱逐，参数为缓存组指针cache_set
+int evict(cache_ *cache, int set_index)
+{ // 最高效的写法应该用哈希表+双向链表，但是这里只能用c语言，hash表需要自己实现，就先直接通过遍历上一次时间戳来实现了
+    int min_time = 0x3f3f3f3f, evict_idx = 0;
+    for (int i = 0; i < cache->E; i++)
+    {
+        if (cache->cache_set[set_index].cache_line[i].last_vis_time < min_time)
+        {
+            min_time = cache->cache_set[set_index].cache_line[i].last_vis_time;
+            evict_idx = i;
+        }
+    }
+    return evict_idx;
+}
+// 数据加载到缓存，参数为缓存组指针cache_set，标记位tag
+int insert(cache_ *cache, int set_index, int tag, int time)
+{
+    int line_idx = 0;
+    if (IsFull(cache, set_index))
+    {
+        line_idx = evict(cache, set_index);
+        cache->cache_set[set_index].cache_line[line_idx].tag = tag;
+        cache->cache_set[set_index].cache_line[line_idx].last_vis_time = time;
+        return 1;
+    }
+    for (int i = 0; i < cache->E; i++)
+    {
+        if (cache->cache_set[set_index].cache_line[i].valid == 0)
+        {
+            line_idx = i;
+            break;
+        }
+    }
+    cache->cache_set[set_index].cache_line[line_idx].valid = 1;
+    cache->cache_set[set_index].cache_line[line_idx].tag = tag;
+    cache->cache_set[set_index].cache_line[line_idx].last_vis_time = time;
+    return 0;
+}
+
+// 写出了缓存需要的函数定义后，先不着急实现，先对把对trace指令的读取处理实现一下
+int change2num(char *s)
+{
+    int res = 0;
+    for (int i = 0; s[i]; i++)
+    {
+        res *= 10;
+        res += s[i] - '0';
+    }
+    return res;
+}
+
+void solveTrace(int s, int E, int b, FILE *fp)
+{
+    cache_ *cache;
+    cache = (cache_ *)malloc(sizeof(cache_));
+    init(cache, s, E, b);
+    int hit = 0, miss = 0, eviction = 0;
+    int time = 0; // 时间戳
+
+    // 处理文件输入
+    char opt[31];
+    // fgets(char* s,int n,FILE *stream) 从stream读取字符串，读取到n个字符，或者读取到一行末尾，或者读取到文件末尾则结束
+    while (fgets(opt, 30, fp))
+    {
+        time++;
+        // printf("%s", opt);
+        char c[2];
+        int addr, size;
+        sscanf(opt, "%s %x,%d", c, &addr, &size);
+        // printf("%s %d %d\n", c, addr, size);
+
+        int tag = addr >> (s + b);                                  // 标记位
+        int set_index = (addr >> b) & ((unsigned)(-1) >> (32 - s)); // 组编号
+
+        if (c[0] == 'L' || c[0] == 'S')
+        {
+            if (find(cache, set_index, tag, time))
+            {
+                hit++;
+                // printf("%s %x,%d hit\n", c, addr, size);
+            }
+            else
+            {
+                miss++;
+                // printf("%s %x,%d miss ", c, addr, size);
+                if (insert(cache, set_index, tag, time))
+                {
+                    eviction++;
+                    // printf("eviction");
+                }
+                // printf("\n");
+            }
+        }
+        else if (c[0] == 'M')
+        {
+            if (find(cache, set_index, tag, time))
+            {
+                hit += 2;
+                // printf("%s %x,%d hit hit\n", c, addr, size);
+            }
+            else
+            {
+                miss++;
+                // printf("%s %x,%d miss ", c, addr, size);
+                if (insert(cache, set_index, tag, time))
+                {
+                    eviction++;
+                    // printf("eviction ");
+                }
+                hit++;
+                // printf("hit\n");
+            }
+        }
+    }
+    printSummary(hit, miss, eviction);
+    del(cache);
+}
+
+void solve(int argc, char *argv[])
+{
+    int s, E, b;
+    FILE *fp; // 文件指针
+    for (int i = 1; i < argc; i++)
+    {
+        // printf("%d %s\n", i, argv[i]);
+        if (argv[i][0] == '-')
+        {
+            if (argv[i][1] == 's')
+            {
+                i++;
+                s = change2num(argv[i]);
+            }
+            else if (argv[i][1] == 'E')
+            {
+                i++;
+                E = change2num(argv[i]);
+            }
+            else if (argv[i][1] == 'b')
+            {
+                i++;
+                b = change2num(argv[i]);
+            }
+            else if (argv[i][1] == 't')
+            {
+                i++;
+                printf("%s\n", argv[i]);
+                fp = fopen(argv[i], "r");
+            }
+        }
+    }
+    solveTrace(s, E, b, fp);
+}
+
+int main(int argc, char *argv[]) // argc为参数数量、argv为参数，其中argv[0]为程序名
+{
+    solve(argc, argv);
+    // printSummary(0, 0, 0);
     return 0;
 }
--- labs/cachelab/trans.c
+++ labs/cachelab/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,30 +6,290 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, k, l;
+    int temp[8];
+    if (M == 32 && N == 32)
+    {
+        for (i = 0; i < N; i += 8)
+        {
+            for (j = 0; j < M; j += 8)
+            {
+
+                if (i != j) // 非对角线子矩阵
+                {
+                    for (k = i; k < i + 8; k++)     // 子矩阵的8行
+                        for (l = j; l < j + 8; l++) // 子矩阵的8列
+                            B[l][k] = A[k][l];
+                }
+                else // i==j，对角线子矩阵
+                {
+                    for (k = i; k < i + 8; k++) // 子矩阵的8行
+                    {
+                        for (l = j; l < j + 8; l++) // 子矩阵的8列
+                            temp[l - j] = A[k][l];  // 取数组A的第k行暂存
+                        for (l = j; l < j + 8; l++)
+                        {
+                            if (l < j + (k - i)) // 对数组B的第k行前k-i个元素以及它们的转置位置的元素进行赋值
+                            {
+                                B[k][l] = B[l][k];
+                                B[l][k] = temp[l - j];
+                            }
+                            else
+                                B[k][l] = temp[l - j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else if (M == 64 && N == 64)
+    {
+
+        for (j = 0; j < M; j += 8)
+        {
+            i = j; // i==j，对角线子矩阵
+
+            for (k = i + 4; k < i + 8; k++) // 先把矩阵的后4行转移位置
+            {
+                // 按行暂存到数组B相邻子矩阵的对应行
+                for (l = j; l < j + 8; l++)
+                    if (j == 0) // 第一行第一个对角线子矩阵
+                        B[k - 4][l + 8] = A[k][l];
+                    else // 其他对角线子矩阵
+                        B[k - 4][l - 8] = A[k][l];
+            }
+            for (k = i; k < i + 4; k++) // 矩阵的前4行按行暂存到数组B的对应行
+            {
+                for (l = j; l < j + 8; l++)
+                    temp[l - j] = A[k][l];
+                for (l = j; l < j + 8; l++)
+                    B[k][l] = temp[l - j];
+            }
+            for (k = i; k < i + 4; k++) // 对B数组上方的两个子矩阵进行转置，注意这里循环只能循环一半元素，否则每个位置会交换两次
+            {
+                for (l = j; l < j + k - i; l++) // 对左上角子矩阵进行转置处理
+                {
+                    temp[0] = B[k][l];
+                    B[k][l] = B[l][k];
+                    B[l][k] = temp[0];
+                }
+                for (l = j + 4; l < j + 4 + k - i; l++) // 对右上角子矩阵进行转置处理
+                {
+                    // k=i，l=j+4 => l-j-4+i，k-i+j+4
+                    temp[0] = B[k][l];
+                    B[k][l] = B[l - j - 4 + i][k - i + j + 4];
+                    B[l - j - 4 + i][k - i + j + 4] = temp[0];
+                }
+            }
+            // 然后套用非对角线子矩阵的逻辑就可以了
+            for (l = j; l < j + 4; l++) // 后4行，按列遍历，先遍历前4列
+            {
+                for (k = i + 4; k < i + 8; k++) // 先把右上角暂存的一行4个值取出来
+                    temp[k - i - 4] = B[l][k];
+                for (k = i + 4; k < i + 8; k++) // 后4行前4列，给右上角转置位置的对应行赋值
+                    if (j == 0)                 // 第一行第一个对角线子矩阵
+                        B[l][k] = B[k - 4][l + 8];
+                    else // 其他对角线子矩阵
+                        B[l][k] = B[k - 4][l - 8];
+                for (k = i + 4; k < i + 8; k++) // 再给左下角当前行前4个赋值
+                    B[l + 4][k - 4] = temp[k - i - 4];
+            }
+            for (l = j + 4; l < j + 8; l++)
+                for (k = i + 4; k < i + 8; k++) // 后4行后4列
+                    if (j == 0)
+                        B[l][k] = B[k - 4][l + 8];
+                    else
+                        B[l][k] = B[k - 4][l - 8];
+
+            for (i = 0; i < N; i += 8)
+            {
+                if (i != j) // 非对角线子矩阵
+                {
+                    for (k = i; k < i + 4; k++) // 前4行
+                    {
+                        for (l = j; l < j + 4; l++) // 前4行前4列，直接转置位置赋值
+                            B[l][k] = A[k][l];
+                        for (l = j + 4; l < j + 8; l++) // 前4行后4列，暂存在子矩阵右上方
+                            B[l - 4][k + 4] = A[k][l];
+                    }
+                    for (l = j; l < j + 4; l++) // 后4行，按列遍历，先遍历前4列
+                    {
+                        for (k = i + 4; k < i + 8; k++) // 先把右上角暂存的一行4个值取出来
+                            temp[k - i - 4] = B[l][k];
+                        for (k = i + 4; k < i + 8; k++) // 后4行前4列，给右上角转置位置的对应行赋值
+                            B[l][k] = A[k][l];
+                        for (k = i + 4; k < i + 8; k++) // 再给左下角当前行前4个赋值
+                            B[l + 4][k - 4] = temp[k - i - 4];
+                    }
+                    for (l = j + 4; l < j + 8; l++)
+                        for (k = i + 4; k < i + 8; k++) // 后4行后4列
+                            B[l][k] = A[k][l];
+                }
+            }
+        }
+    }
+    else
+    {
+        for (i = 0; i < N; i += 16)
+        {
+            for (j = 0; j < M; j += 16)
+            {
+                for (k = i; k < (i + 16 >= N ? N : i + 16); k++)     // 子矩阵的8行
+                    for (l = j; l < (j + 16 >= M ? M : j + 16); l++) // 子矩阵的8列
+                        B[l][k] = A[k][l];
+            }
+        }
+    }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
+char transpose_32_plus_32_desc[] = "32 plus 32 matrix";
+void transpose_32_plus_32(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, k, l;
+    int temp[8];
+    for (i = 0; i < N; i += 8)
+    {
+        for (j = 0; j < M; j += 8)
+        {
 
-/* 
+            if (i != j) // 非对角线子矩阵
+            {
+                for (k = i; k < i + 8; k++)     // 子矩阵的8行
+                    for (l = j; l < j + 8; l++) // 子矩阵的8列
+                        B[l][k] = A[k][l];
+            }
+            else // i==j，对角线子矩阵
+            {
+                for (k = i; k < i + 8; k++) // 子矩阵的8行
+                {
+                    for (l = j; l < j + 8; l++) // 子矩阵的8列
+                        temp[l - j] = A[k][l];  // 取数组A的第k行暂存
+                    for (l = j; l < j + 8; l++)
+                    {
+                        if (l < j + (k - i)) // 对数组B的第k行前k-i个元素以及它们的转置位置的元素进行赋值
+                        {
+                            B[k][l] = B[l][k];
+                            B[l][k] = temp[l - j];
+                        }
+                        else
+                            B[k][l] = temp[l - j];
+                    }
+                }
+            }
+        }
+    }
+}
+
+char transpose_64_plus_64_desc[] = "64 plus 64 matrix";
+void transpose_64_plus_64(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, k, l;
+    int temp[8];
+    for (j = 0; j < M; j += 8)
+    {
+        i = j; // i==j，对角线子矩阵
+
+        for (k = i + 4; k < i + 8; k++) // 先把矩阵的后4行转移位置
+        {
+            // 按行暂存到数组B相邻子矩阵的对应行
+            for (l = j; l < j + 8; l++)
+                if (j == 0) // 第一行第一个对角线子矩阵
+                    B[k - 4][l + 8] = A[k][l];
+                else // 其他对角线子矩阵
+                    B[k - 4][l - 8] = A[k][l];
+        }
+        for (k = i; k < i + 4; k++) // 矩阵的前4行按行暂存到数组B的对应行
+        {
+            for (l = j; l < j + 8; l++)
+                temp[l - j] = A[k][l];
+            for (l = j; l < j + 8; l++)
+                B[k][l] = temp[l - j];
+        }
+        for (k = i; k < i + 4; k++) // 对B数组上方的两个子矩阵进行转置，注意这里循环只能循环一半元素，否则每个位置会交换两次
+        {
+            for (l = j; l < j + k - i; l++) // 对左上角子矩阵进行转置处理
+            {
+                temp[0] = B[k][l];
+                B[k][l] = B[l][k];
+                B[l][k] = temp[0];
+            }
+            for (l = j + 4; l < j + 4 + k - i; l++) // 对右上角子矩阵进行转置处理
+            {
+                // k=i，l=j+4 => l-j-4+i，k-i+j+4
+                temp[0] = B[k][l];
+                B[k][l] = B[l - j - 4 + i][k - i + j + 4];
+                B[l - j - 4 + i][k - i + j + 4] = temp[0];
+            }
+        }
+        // 然后套用非对角线子矩阵的逻辑就可以了
+        for (l = j; l < j + 4; l++) // 后4行，按列遍历，先遍历前4列
+        {
+            for (k = i + 4; k < i + 8; k++) // 先把右上角暂存的一行4个值取出来
+                temp[k - i - 4] = B[l][k];
+            for (k = i + 4; k < i + 8; k++) // 后4行前4列，给右上角转置位置的对应行赋值
+                if (j == 0)                 // 第一行第一个对角线子矩阵
+                    B[l][k] = B[k - 4][l + 8];
+                else // 其他对角线子矩阵
+                    B[l][k] = B[k - 4][l - 8];
+            for (k = i + 4; k < i + 8; k++) // 再给左下角当前行前4个赋值
+                B[l + 4][k - 4] = temp[k - i - 4];
+        }
+        for (l = j + 4; l < j + 8; l++)
+            for (k = i + 4; k < i + 8; k++) // 后4行后4列
+                if (j == 0)
+                    B[l][k] = B[k - 4][l + 8];
+                else
+                    B[l][k] = B[k - 4][l - 8];
+
+        for (i = 0; i < N; i += 8)
+        {
+            if (i != j) // 非对角线子矩阵
+            {
+                for (k = i; k < i + 4; k++) // 前4行
+                {
+                    for (l = j; l < j + 4; l++) // 前4行前4列，直接转置位置赋值
+                        B[l][k] = A[k][l];
+                    for (l = j + 4; l < j + 8; l++) // 前4行后4列，暂存在子矩阵右上方
+                        B[l - 4][k + 4] = A[k][l];
+                }
+                for (l = j; l < j + 4; l++) // 后4行，按列遍历，先遍历前4列
+                {
+                    for (k = i + 4; k < i + 8; k++) // 先把右上角暂存的一行4个值取出来
+                        temp[k - i - 4] = B[l][k];
+                    for (k = i + 4; k < i + 8; k++) // 后4行前4列，给右上角转置位置的对应行赋值
+                        B[l][k] = A[k][l];
+                    for (k = i + 4; k < i + 8; k++) // 再给左下角当前行前4个赋值
+                        B[l + 4][k - 4] = temp[k - i - 4];
+                }
+                for (l = j + 4; l < j + 8; l++)
+                    for (k = i + 4; k < i + 8; k++) // 后4行后4列
+                        B[l][k] = A[k][l];
+            }
+        }
+    }
+}
+
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
@@ -37,13 +297,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -56,14 +317,16 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    registerTransFunction(trans, trans_desc);
 
+    // registerTransFunction(transpose_32_plus_32, transpose_32_plus_32_desc);
+    // registerTransFunction(transpose_64_plus_64, transpose_64_plus_64_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -72,13 +335,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
